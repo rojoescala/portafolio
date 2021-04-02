58cb2be5f21ff33af4720e92e065c5c55cd31225
rojo03 <rojo.escala@gmail.com>
2021-04-02 11:13:34 -0700

    Avances de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { SocialSidebarComponent } from './social-sidebar/social-sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SocialSidebarComponent,
+    AboutMeComponent,
+    WelcomeComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/social-sidebar/social-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'social-sidebar',
+  templateUrl: './social-sidebar.component.html',
+  styleUrls: ['./social-sidebar.component.css']
+})
+export class SocialSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
